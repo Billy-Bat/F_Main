f3aca2a5b6050822ad760a775f9cc7c29ce6bd4d
Billy_Bat <gguillaume.gonde@gmail.com>
2019-11-20 18:57:13 +0900

    Added plot of the Model evaluation and residual evaluation

--- Stock_Forecast.py
+++ Stock_Forecast.py
@@ -11,24 +11,27 @@ from pandas_datareader import data
 
 # STATSMODELS import
 import statsmodels.api as sm
+from statsmodels.stats.diagnostic import acorr_ljungbox
 from statsmodels.tsa.statespace.sarimax import SARIMAX
+from statsmodels.tsa.stattools import adfuller
 from statsmodels.graphics.gofplots import qqplot
 import statsmodels.graphics.tsaplots as tsa
 
 # MATPLOTLIB import
 import matplotlib
+import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
 plt.style.use('fivethirtyeight')
 matplotlib.rcParams["axes.labelsize"] = 14
-matplotlib.rcParams["xtick.labelsize"] = 10
-matplotlib.rcParams["ytick.labelsize"] = 10
+matplotlib.rcParams["xtick.labelsize"] = 8
+matplotlib.rcParams["ytick.labelsize"] = 8
 matplotlib.rcParams["text.color"] = 'k'
 matplotlib.rcParams["font.size"] = 5
-matplotlib.rcParams["xtick.labelsize"] = 7
+matplotlib.rcParams["xtick.labelsize"] = 5
 matplotlib.rcParams["ytick.labelsize"] = 7
 matplotlib.rcParams["lines.linewidth"] = 0.5
 matplotlib.rcParams["figure.subplot.hspace"] = 0.5
-matplotlib.rcParams["axes.labelsize"] = 2
+matplotlib.rcParams["axes.labelsize"] = 6
 matplotlib.rcParams["lines.markersize"] = 0.5
 matplotlib.rcParams['figure.figsize'] = 15, 7
 
@@ -84,15 +87,33 @@ def evaluate_model(Train_set, Test_set, pdq_val, PDQ_val, _print=True, _plot=Tru
         history.append(Test_set.iloc[step,0])
 
     error = mean_squared_error(Test_set.iloc[:,0], predictions)
+    Error_Array = Test_set.iloc[:,0] - predictions
 
     if _print :
         print('Config{}{} MSE : {}'.format(pdq_val, PDQ_val, error))
 
     if _plot == True :
+        fig = plt.figure()
+        fig.suptitle('SARIMA{}{} Model Analysis'.format(pdq_val, PDQ_val), size=14)
+        gs = gridspec.GridSpec(3, 2) # For some reason add_gridspec DW
+        ax1 = plt.subplot(gs[:,0])
+        ax2 = plt.subplot(gs[0,1])
+        ax3 = plt.subplot(gs[1,1])
+        ax4 = plt.subplot(gs[2,1])
+
+        #1# Rolling Forecast
         Actual = pd.concat([Train_set.iloc[:,0], Test_set.iloc[:,0]])
-        ax = Actual.plot()
-        pd.DataFrame(predictions ,index=Test_set.index).plot(ax=ax)
-        ax.legend(['Actual', 'Rolling forcast'])
+        Actual.plot(ax=ax1)
+        pd.DataFrame(predictions ,index=Test_set.index).plot(ax=ax1)
+        ax1.legend(['Actual', 'Rolling forcast'])
+        #2# Residual Distrib
+        Error_Array.plot(ax=ax2, kind='kde')
+        ax2.set_title('Residual Distribution')
+        #3# Residual ACF
+        tsa.plot_acf(Error_Array, ax=ax3, lags=12, alpha=0.05)
+        #4# Residual PACF
+        tsa.plot_pacf(Error_Array, ax=ax4, lags=12, alpha=0.05)
+
         plt.show()
 
 
@@ -105,7 +126,8 @@ def plot_Model_Identify(DataSet, frequency=1, acf_lag=12, pacf_lag=12):
     Frequency : int, Seasonal Component period (in time step)
     """
     # Organize plot
-    fig, ax = plt.subplots(3, 4)
+    fig, ax = plt.subplots(3, 5)
+    fig.suptitle('Model Identification Season length :{}'.format(frequency), size=14)
 
     # Plot the Observed Data
     DataSet.plot(ax=ax[0,0])
@@ -139,18 +161,69 @@ def plot_Model_Identify(DataSet, frequency=1, acf_lag=12, pacf_lag=12):
     decomposition.resid.plot(ax=ax[0,2])
     try :
       decomposition.resid.plot(ax=ax[0,3], kind='kde')
+      tsa.plot_acf(decomposition.resid.dropna(), ax=ax[1,3], lags=acf_lag*2, alpha=0.05)
+      tsa.plot_pacf(decomposition.resid.dropna(), ax=ax[2,3], lags=pacf_lag*2, alpha=0.05)
     except :
         print('WARNING : Residual Probability plot Failed')
         print(sys.exc_info()[0])
 
     decomposition.seasonal.plot(ax=ax[1,2])
     decomposition.trend.plot(ax=ax[2,2])
+    Ljung_BoxTest(decomposition.resid, frequency)
+
+    # lag_plot(decomposition.seasonal.dropna(), ax=ax[0,4]) #Seasonality doesnt need DIFF, i.e it has no (obvious) trend
+    tsa.plot_acf(decomposition.seasonal, ax=ax[0,4], lags=acf_lag, alpha=0.05)
+    tsa.plot_pacf(decomposition.seasonal, ax=ax[1,4], lags=pacf_lag, alpha=0.05)
+
+
     ax[0, 2].set_title('Residual')
     ax[1, 2].set_title('Seasonal')
     ax[2, 2].set_title('Trend')
     ax[0, 3].set_title('Residual Prob. Distrib')
+    ax[1,3].set_title('Residual ACF')
+    ax[2,3].set_title('Residual PACF')
 
     plt.show()
+    return 1
+
+def DFuller_Test(Data, _print=True):
+    """
+    Perform Dickey Fuller on the seasonal/Residual Data
+    Identify if the Data is defined by a trend
+    p < 0.05 Data is stationary
+    p > 0.05 Data is non-stationary
+    """
+    Data = Data.dropna() # Some NaN entries
+    X = Data.values
+    result = adfuller(X)
+    if _print :
+        print('ADF Result: {}'.format(result[0]))
+        print('p-value: {}'.format(result[1]))
+        for key, value in result[4].items():
+            print('Critical Value {}: {}'.format(key, value))
+
+    return result
+
+def Ljung_BoxTest(Data, lag, BP=False, _print=True,):
+    """
+    Perform a Ljung-Box Test on the Residual Data up to lag
+    By Default use lag=Season_Periodx2
+    Test only check if values are dependent, doesnt assert that values are independant
+    """ # CHECKS FOR NO AUTOCORELLATION
+    try :
+        X = Data.dropna()
+        Res = acorr_ljungbox(X, lags=lag, boxpierce=BP)
+    except :
+        print('WARNING : Ljung_BoxTest Failed')
+        print(sys.exc_info()[0])
+
+    if _print :
+        print('Ljung-Box value: {}'.format(Res[0].mean()))
+        print('LB p value: {}'.format(Res[1].mean()))
+        if BP :
+            print('Box-Pierce value: {}'.format(Res[2]))
+            print('BP p value: {}'.format(Res[3]))
+
 
 ################################################################################
 #1# Get Data
@@ -170,55 +243,51 @@ df.dropna()
 mean = df.resample('M').mean()
 
 #2# Identify model charaterstics
-# plot_Model_Identify(df, frequency=45, acf_lag=12, pacf_lag=12)
+# plot_Model_Identify(df, frequency=15, acf_lag=20, pacf_lag=20)
 
 #3# Divid the set into training and test
 Training_set, Test_set = train_test_split(df, 24)
 
 #4# Grid Search Optimal parameters
-pdq, PDQ = Get_Config([(2,5), (0,2), (0,2)], [(0,0), (0,0), (0,0)], [45])
-
-min_BIC = float('inf') # quite arbitrary
-min_AIC = float('inf')
-min_sharedBIC = float('inf')
-min_sharedAIC = float('inf')
-
-top_BICParam = []
-top_AICParam = []
-top_shared = []
-
-for pdq_param in pdq :
-    for PDQ_param in PDQ :
-        mod = sm.tsa.statespace.SARIMAX(Training_set.iloc[:,0],
-                                                order=pdq_param,
-                                                seasonal_order=PDQ_param,
-                                                enforce_stationarity=False,
-                                                enforce_invertibility=False,
-                                                disp=False)
-        result = mod.fit(disp=False)
-        print('Model pdq :{}, PDQ :{} AIC : {}'.format(pdq_param, PDQ_param, result.bic))
-
-        if result.bic < min_BIC :
-            min_BIC = result.bic
-            top_BICParam = [pdq_param, PDQ_param]
-        if result.aic < min_AIC :
-            min_AIC = result.aic
-            top_AICParam = [pdq_param, PDQ_param]
-        if result.bic < min_sharedBIC and result.aic < min_sharedAIC :
-            min_sharedBIC = result.bic
-            min_sharedAIC = result.aic
-            top_shared = [pdq_param, PDQ_param]
-
-
-
-print('Top AIC Config : {} with AIC : {}'.format(top_AICParam, min_AIC))
-print('Top BIC Config : {} with BIC : {}'.format(top_BICParam, min_BIC))
-print('Shared top Config : {} with BIC : {} AIC : {}'.format(top_shared, min_sharedBIC, min_sharedAIC))
+# pdq, PDQ = Get_Config([(2,5), (5,8), (0,2)], [(0,0), (0,0), (0,0)], [45])
+#
+# min_BIC = float('inf') # quite arbitrary
+# min_AIC = float('inf')
+# min_sharedBIC = float('inf')
+# min_sharedAIC = float('inf')
+#
+# top_BICParam = []
+# top_AICParam = []
+# top_shared = []
+#
+# for pdq_param in pdq :
+#     for PDQ_param in PDQ :
+#         mod = sm.tsa.statespace.SARIMAX(Training_set.iloc[:,0],
+#                                                 order=pdq_param,
+#                                                 seasonal_order=PDQ_param,
+#                                                 enforce_stationarity=False,
+#                                                 enforce_invertibility=False,
+#                                                 disp=False)
+#         result = mod.fit(disp=False)
+#         print('Model pdq :{}, PDQ :{} AIC : {}'.format(pdq_param, PDQ_param, result.bic))
+#
+#         if result.bic < min_BIC :
+#             min_BIC = result.bic
+#             top_BICParam = [pdq_param, PDQ_param]
+#         if result.aic < min_AIC :
+#             min_AIC = result.aic
+#             top_AICParam = [pdq_param, PDQ_param]
+#         if result.bic < min_sharedBIC and result.aic < min_sharedAIC :
+#             min_sharedBIC = result.bic
+#             min_sharedAIC = result.aic
+#             top_shared = [pdq_param, PDQ_param]
+# print('Top AIC Config : {} with AIC : {}'.format(top_AICParam, min_AIC))
+# print('Top BIC Config : {} with BIC : {}'.format(top_BICParam, min_BIC))
+# print('Shared top Config : {} with BIC : {} AIC : {}'.format(top_shared, min_sharedBIC, min_sharedAIC))
 
 
-# evaluate_model(Training_set, Test_set, (2, 1, 4), (0, 0, 0, 45))
-
-
+#5# Evaluate Model(s)
+evaluate_model(Training_set, Test_set, (3, 0, 1), (0, 0, 0, 80))
 
 
 """
